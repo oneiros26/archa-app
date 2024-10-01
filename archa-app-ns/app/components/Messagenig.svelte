95d5a52406c5d9a5ea71791e017e7bbf63318bf7
foglar <kohout.fi.2023@skola.ssps.cz>
2024-10-01 09:17:37 +0200

    messagenig

--- archa-app-ns/app/components/Messagenig.svelte
+++ archa-app-ns/app/components/Messagenig.svelte
@@ -1,13 +1,18 @@
 <script>
-	import { Button, TextField } from '@nativescript/core';
+	import { Button, Label } from '@nativescript/core';
+	import { get } from 'svelte/store';
+	let textElem;
+	let textMsg;
 
-	// Click button, read textfield, send message
-	Button.on('tap', () => {
-        const textField = TextField;
-        // get the message from the text field
-        const message = textField.text;
-        // add new message to the chat
-    });
+	Button.on('tap', sendMessage);
+
+	function sendMessage() {
+		if (textElem) {
+			// Update the label with the message and make it visible
+			textMsg.text = textElem;
+			console.log(textElem);
+		}
+	}
 </script>
 
 <absoluteLayout>
@@ -25,25 +30,25 @@
 		top="150"
 		text="You can also receive messages."
 		backgroundColor="#76a8ff"
-        marginTop="30"
-        />
-    <label
-        left="0"
-        top="200"
-        text="What a nice conversation."
-        backgroundColor="#64d13e"
-        marginTop="50"
-    />
-    <label
-        left="212"
-        top="250"
-        text="Skibidi rizz."
-        backgroundColor="#76a8ff"
-        marginTop="70"
-    />
-
+		marginTop="30"
+	/>
+	<label
+		left="0"
+		top="200"
+		text="What a nice conversation."
+		backgroundColor="#64d13e"
+		marginTop="50"
+	/>
+	<label
+		bind:this={textMsg}
+		left="212"
+		top="250"
+		text={textElem}
+		backgroundColor="#76a8ff"
+		marginTop="70"
+	/>
 	<stackLayout left="0" top="300" width="100%" height="100%" orientation="horizontal">
-		<textField left="0" top="620" hint="Type your message here" />
+		<textField left="0" top="620" bind:text={textElem} hint="Type your message here" />
 		<button left="0" top="620" text=""> </button>
 	</stackLayout>
 </absoluteLayout>
