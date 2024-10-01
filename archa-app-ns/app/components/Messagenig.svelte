3b02b0b6cb6b8e30f9edebaf8e2d7bad6f882dd4
Sidlich508 <samuel.sidlichovsky@gmail.com>
2024-10-01 10:06:20 +0200

    Mesanigga funguje

--- archa-app-ns/app/components/Messagenig.svelte
+++ archa-app-ns/app/components/Messagenig.svelte
@@ -1,55 +1,36 @@
 <script>
-	import { Button, Label } from '@nativescript/core';
-	import { get } from 'svelte/store';
-	let textElem;
-	let textMsg;
-
-	Button.on('tap', sendMessage);
+	import { writable } from 'svelte/store';
+	let textElem = ''; // Message input
+	let messages = writable([]); // Array to store the messages
 
 	function sendMessage() {
 		if (textElem) {
-			// Update the label with the message and make it visible
-			textMsg.text = textElem;
-			console.log(textElem);
+			// Add the new message to the messages array
+			messages.update((msgs) => [...msgs, textElem]);
+			console.log(textElem); // Log the entered message
+			textElem = ''; // Clear the input after sending
 		}
 	}
 </script>
 
 <absoluteLayout>
-	<label left="0" top="0" text="Hi, want to get some bitches?" backgroundColor="#64d13e" />
-	<label left="0" top="50" text="This is the messaging page." backgroundColor="#64d13e" />
-	<label
-		left="50"
-		top="100"
-		text="You can send messages here."
-		backgroundColor="#76a8ff"
-		marginTop="30"
-	/>
-	<label
-		left="30"
-		top="150"
-		text="You can also receive messages."
-		backgroundColor="#76a8ff"
-		marginTop="30"
-	/>
-	<label
-		left="0"
-		top="200"
-		text="What a nice conversation."
-		backgroundColor="#64d13e"
-		marginTop="50"
-	/>
-	<label
-		bind:this={textMsg}
-		left="212"
-		top="250"
-		text={textElem}
-		backgroundColor="#76a8ff"
-		marginTop="70"
-	/>
+	<label left="110" top="0" text="Hi, want to get some bitches?" backgroundColor="#64d13e" />
+	<label left="123" top="50" text="This is the messaging page." backgroundColor="#64d13e" />
+	
+	<!-- This block dynamically generates new labels for each message -->
+	{#each $messages as message, i}
+		<label
+			right="0"
+			top={100 + (i * 50)}
+			text={message}
+			backgroundColor="#76a8ff"
+			marginTop="10"
+		/>
+	{/each}
+
 	<stackLayout left="0" top="300" width="100%" height="100%" orientation="horizontal">
 		<textField left="0" top="620" bind:text={textElem} hint="Type your message here" />
-		<button left="0" top="620" text=""> </button>
+		<button left="0" top="620" text="" on:tap={sendMessage}> </button>
 	</stackLayout>
 </absoluteLayout>
 
