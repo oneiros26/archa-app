7eca3b95297a0aeffa2034d05e1c4f70894b8494
Vít Sobíšek <sobisekvit@gmail.com>
2024-10-01 03:47:35 +0200

    Finished v1 Ticket.svelte

--- archa-app-ns/app/components/Ticket.svelte
+++ archa-app-ns/app/components/Ticket.svelte
@@ -1,10 +1,116 @@
 <script>
 
 </script>
-<gridLayout rows="auto, auto, auto" columns="auto, auto, auto" class="ticket">
-    <label textWrap="true">Ticket</label>
-</gridLayout>
+<scrollView>
+    <flexboxLayout flexDirection="column" class="vertical-gradient-background">
+        <label textWrap="true" height="80" textAlignment="center">
+            <formattedString>
+                <span text=" ARCHA+ Diamond" fontweight="200" style="font-family: 'Archabeta Regular', 'archabeta-regular'" fontSize="45"/>
+            </formattedString>
+          </label>
+        <label textWrap="true" height="85" textAlignment="center">
+            <formattedString>
+                <span text=" Divadlo bez " fontWeight="900" style="font-family: 'Archabeta Regular', 'archabeta-regular'" fontSize="80"/>
+            </formattedString>
+          </label>
+        <label textWrap="true" height="85" textAlignment="center">
+            <formattedString>
+                <span text="starostí. " fontWeight="900" style="font-family: 'Archabeta Regular', 'archabeta-regular'" fontSize="80"/>
+            </formattedString>
+          </label>
+        <label textWrap="true" height="140" textAlignment="center">
+            <formattedString>
+                <span text="Předností objednaní a slevy na divadelní hry, slevy na jídlo, dárek k narozeninám." fontWeight="400" style="font-family: 'Archabeta Regular', 'archabeta-regular'" fontSize="22"/>
+            </formattedString>
+          </label>
+    
+        <button class="farch-reg buy-button" text="Koupit na 6 měsíců" />
+    
+        <label textWrap="true" height="20" textAlignment="">
+            <formattedString>
+                <span text=""/>
+            </formattedString>
+          </label>
+        <label textWrap="true" textAlignment="left">
+            <formattedString>
+                <span text=" 
+                 - Přednostní objednaní
+    
+                 - Slevy na divadelní hry
+    
+                 - Slevy na jídlo
+    
+                 - Dárek k narozeninám
+                 
+                 - Jedna vstupenka zdarma" fontWeight="400" effectiveMarginLeft="1px" style="font-family: 'Archabeta Bold', 'archabeta-bold'" fontSize="25"/>
+            </formattedString>
+          </label>
+        <label textWrap="true" height="50" textAlignment="">
+            <formattedString>
+                <span text=""/>
+            </formattedString>
+          </label>
+          <label textWrap="true" height="50" textAlignment="left" class="fas" fontSize="30" marginLeft="45px" marginTop="20px" marginBottom="0px">
+            <span text=" Individualní"/>
+          </label>
+        <label class="line-divider" />
+        <label textWrap="true" textAlignment="left" class="farcha-reg" fontSize="18" marginLeft="45px" marginTop="10px" marginBottom="0px">
+            <span text="CZK XXX.XX/měsíc"/>
+        </label>
+        <button class="farch-reg buy-button" text="Koupit na 6 měsíců" />
+        <button class="farch-reg buy-button" text="Koupit na 12 měsíců" />
+
+        <label textWrap="true" height="50" textAlignment="left" class="fas" fontSize="30" marginLeft="45px" marginTop="20px" marginBottom="0px">
+            <span text=" Rodinné"/>
+          </label>
+        <label class="line-divider" />
+        <label textWrap="true" textAlignment="left" class="farcha-reg" fontSize="18" marginLeft="45px" marginTop="10px" marginBottom="0px">
+            <span text="CZK XXX.XX/měsíc"/>
+        </label>
+        <button class="farch-reg buy-button" text="Koupit na 6 měsíců" />
+        <button class="farch-reg buy-button" text="Koupit na 12 měsíců" />
+
+        <label textWrap="true" height="50" textAlignment="left" class="fas" fontSize="30" marginLeft="45px" marginTop="20px" marginBottom="0px">
+            <span text=" Studentské"/>
+          </label>
+        <label class="line-divider" />
+        <label textWrap="true" textAlignment="left" class="farcha-reg" fontSize="18" marginLeft="45px" marginTop="10px" marginBottom="0px">
+            <span text="CZK XXX.XX/měsíc"/>
+        </label>
+        
+        <button class="farch-reg buy-button" text="Koupit na 6 měsíců" />
+        <button class="farch-reg buy-button" text="Koupit na 12 měsíců" />
+
+        <label textWrap="true" height="100" textAlignment="">
+            <formattedString>
+                <span text=""/>
+            </formattedString>
+          </label>
+
+      </flexboxLayout>
+
+    
+</scrollView>
 
 <style>
-    
+
+    .vertical-gradient-background {
+    background: linear-gradient(180deg, #b8ff00 0%, #76a8ff 100%)
+    }
+
+    .buy-button {
+        border-radius: 30px;
+        background: linear-gradient(100deg, #ac71ff 0%, #ff78d9 100%);
+        color: #0f0f0f;
+        font-size: 22px;
+        border-width: 3px;
+        border-color: #0f0f0f;
+    }
+
+    .line-divider {
+    height: 2;
+    background-color: #ccc;
+    margin: 10;
+    }
+
 </style>
